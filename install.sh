c065a7c5b6ea789c565463b064329d3d0501a420
Barry Yu <notlcry@gmail.com>
2020-07-16 11:56:04 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 apt-get -y install strongswan strongswan-plugin-xauth-generic
-
-sysctl -w net.ipv4.ip_forward=1
+wget https://raw.githubusercontent.com/notlcry/strongswanubuntu/master/ipsec.conf -O /etc/ipsec.conf
+wget https://raw.githubusercontent.com/notlcry/strongswanubuntu/master/ipsec.secrets -O /etc/ipsec.secrets
 sysctl -w net.ipv4.ip_forward=1
 iptables -t nat -A POSTROUTING -s 172.31.3.0/24 -o eth0 -j MASQUERADE
 ipsec restart
